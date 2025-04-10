ccad4c9cb53b0b122ac357f4ddac34ad5dc271cb
kennarachael <142414095+kennarachael@users.noreply.github.com>
2025-04-10 15:59:19 -0400

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -3,14 +3,14 @@ take an order from the customer (user input)  and return the price .
 """
 
 class Restaurant
-  def__init__(self):
+  def __init__(self):
     self.prices = [1,2,3,40,50,25,43]
     self.menu = ["Water", "Pop", "Crackers", "Burger", "Steak","Pizza", "Rigatoni"]
     #prices correspond to food items in order
   def order(self):
     pass
     #take an input ( recall the input() function!)
-    #return a string with their price - eg "your price is $___!
+    #return a string with their price - eg "your price is $___!"
 
 
 
